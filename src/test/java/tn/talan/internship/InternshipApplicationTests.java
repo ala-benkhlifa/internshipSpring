4d749e37eee5bda8001ba4b0d299212f04789a84
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-18 15:28:48 +0100

    Initial commit

---
+++ src/test/java/tn/talan/internship/InternshipApplicationTests.java
@@ -0,0 +1,16 @@
+package tn.talan.internship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
