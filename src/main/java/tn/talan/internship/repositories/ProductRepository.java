caa5dd358708807df19806300d4893b4dcf6caa1
chaimaMaiza <chaimamaiza772@gmail.com>
2020-02-18 17:34:34 +0100

    Data base configuration

--- src/main/java/tn/talan/internship/repositories/ProductRepository.java
+++ src/main/java/tn/talan/internship/repositories/ProductRepository.java
@@ -1,7 +1,9 @@
 package tn.talan.internship.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.talan.internship.entities.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
