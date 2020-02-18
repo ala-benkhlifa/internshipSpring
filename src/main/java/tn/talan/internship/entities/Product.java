c6151547b5afb2b407afec97d8c11a2c39512ca9
chaimaMaiza <chaimamaiza772@gmail.com>
2020-02-18 15:56:08 +0100

    remove @requiredArgsConst

--- src/main/java/tn/talan/internship/entities/Product.java
+++ src/main/java/tn/talan/internship/entities/Product.java
@@ -1,6 +1,8 @@
 package tn.talan.internship.entities;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,7 +11,6 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class Product {
     @Id
     private Long id;
