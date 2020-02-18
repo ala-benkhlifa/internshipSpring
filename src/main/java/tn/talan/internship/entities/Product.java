de25e10b316b429b95e29dac30c17b9c43dbe64d
asus <ghadaamri30@gmail.com>
2020-02-18 15:47:48 +0100

    Product entity added

---
+++ src/main/java/tn/talan/internship/entities/Product.java
@@ -0,0 +1,17 @@
+package tn.talan.internship.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Product {
+    @Id
+    private Long id;
+    private Double price;
+}
