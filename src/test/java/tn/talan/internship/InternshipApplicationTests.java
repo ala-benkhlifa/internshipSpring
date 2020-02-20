af7f40cc7fdb55eae4247b3b978e1992d28e4b8c
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-20 16:09:37 +0100

    GitHub Workflow Added with Build feature and h2 database for test purposes added for tests

--- src/test/java/tn/talan/internship/InternshipApplicationTests.java
+++ src/test/java/tn/talan/internship/InternshipApplicationTests.java
@@ -1,12 +1,18 @@
 package tn.talan.internship;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import tn.talan.internship.services.impl.SaleServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 public class InternshipApplicationTests {
 
 	@Test
